d30fbb402026ff47ff97ebf6c8f9483c4119d8ed
Kef Caban <107267432+kefcabz@users.noreply.github.com>
2025-09-25 00:25:47 -0400

    Missile Projectile Script

---
+++ Example_Game/Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float lifetime = 5f;
+
+    void Start()
+    {
+        // Destroy after lifetime expires in case it never hits anything
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Destroy missile when it hits anything
+        Destroy(gameObject);
+    }
+}
