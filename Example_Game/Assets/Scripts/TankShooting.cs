868240acedd27a47b994d1a16d8f90589d8833bb
Kef Caban <107267432+kefcabz@users.noreply.github.com>
2025-09-30 14:44:31 -0400

    Shooting script edit

--- Example_Game/Assets/Scripts/TankShooting.cs
+++ Example_Game/Assets/Scripts/TankShooting.cs
@@ -17,6 +17,8 @@ public class TankShooting : MonoBehaviour
             Shoot();
             nextFireTime = Time.time + 1f / fireRate;
         }
+
+        //Destroy Object on Collision 
     }
 
     void Shoot()
